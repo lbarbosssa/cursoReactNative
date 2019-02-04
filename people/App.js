ba042b5128ba879717aefb1083afc07e99e1d7e6
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-04 16:20:40 -0200

    Componente PeopleList

--- people/App.js
+++ people/App.js
@@ -1,46 +1,39 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header'
+import PeopleList from './src/components/PeopleList'
+
 import axios from 'axios';
 
 
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
       peoples: []
     }
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     axios
-    .get('https://randomuser.me/api?nat=br&results=5')
-    .then(response => {
-      const { results } = response.data
-      this.setState({
-        peoples: results
+      .get('https://randomuser.me/api?nat=br&results=5')
+      .then(response => {
+        const { results } = response.data
+        this.setState({
+          peoples: results
+        })
       })
-    })
   }
 
-  renderList(){
-    const textElements = this.state.peoples.map(people => {
-      const { first } = people.name
-      return <Text key={first}>{ first }</Text>
-    })
 
-    return textElements
-  }
- 
-
-  render (){
+  render() {
     return (
       <View>
-        <Header title="Pessoas!"/>
-        {this.renderList()}
+        <Header title="Pessoas!" />
+        <PeopleList peoples={this.state.peoples} />
       </View>
     )
   }
