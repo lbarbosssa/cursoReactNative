a8e375877cdb9dc54b73c058c2db9ee2bdb82294
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-04 13:38:19 -0200

    Renderizando uma lista

--- people/App.js
+++ people/App.js
@@ -6,10 +6,27 @@ import Header from './src/components/Header'
 
 
 export default class App extends Component {
+  renderList() {
+    const names = [
+      'eddie Van Halen',
+      'Jimi Hendrix',
+      'Chimbinha',
+      'Steve Vai'
+    ]
+
+    const textElemntes = names.map((name, index) => {
+      return <Text key={index}>{name}</Text>
+    })
+
+    return textElemntes 
+    
+  }
+
   render (){
     return (
       <View>
         <Header title="Pessoas!"/>
+        {this.renderList()}
       </View>
     )
   }
