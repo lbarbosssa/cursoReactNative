f0b316160058b782c27762a3cc21c3e19c257db2
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-04 12:34:34 -0200

    Teste: Mudei o texto, para dar uma clonada

--- TESTE/App.js
+++ TESTE/App.js
@@ -13,8 +13,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Esta testado',
 });
 
 type Props = {};
