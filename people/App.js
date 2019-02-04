a87fac7850da9bf4d062e6d253433ba183fd5324
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-04 17:23:29 -0200

    Componente PeopleListitem

--- people/App.js
+++ people/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import Header from './src/components/Header'
 import PeopleList from './src/components/PeopleList'
--- people/src/components/PeopleList.js
+++ people/src/components/PeopleList.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+import PeopleListItem from './PeopleListItem'
+
 const PeopleList = props => {
     const { peoples } = props
-    const textElements = peoples.map(people => {
-        const { first } = people.name
-        return (
-            <View key={first} style={styles.line}>
-                <Text style={styles.lineText}>{first}</Text>
-            </View>
-        )
+
+    const items = peoples.map(people => {
+        return <PeopleListItem key={people.name.first} people={people} />
     })
 
     return (
         <View style={styles.container}>
-            {textElements}
+            { items }
         </View>
     )
 }
@@ -22,19 +20,6 @@ const PeopleList = props => {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#e2f9ff'
-    },
-    line: {
-        height: 60,
-        borderBottomWidth: 1,
-        borderBottomColor: "#bbb",
-
-        alignItems: 'center',
-        flexDirection: 'row'
-
-    },
-    lineText: {
-        fontSize: 20,
-        paddingLeft: 15,
     }
 })
 
