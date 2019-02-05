129b3af57a5a3371d94a34b8f4fe8fa763d65b30
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-05 09:15:11 -0200

    Preparando App.js para navegação

--- people/App.js
+++ people/App.js
@@ -1,3 +1,14 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+
 import PeoplePage from './src/pages/PeoplePage'
 
+const StackNavigator = createStackNavigator ({
+    'Main': {
+      screen: PeoplePage
+    }
+})
+
+const AppContainer = createAppContainer(StackNavigator)
+
 export default PeoplePage
+
