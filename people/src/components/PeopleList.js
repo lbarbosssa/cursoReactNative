1f022f2641610a16198765176f897352aa6e2b5c
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-05 13:34:03 -0200

    Implementando FlatList

--- people/src/components/PeopleList.js
+++ people/src/components/PeopleList.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { ScrollView, Text, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 
 import PeopleListItem from './PeopleListItem'
 
 const PeopleList = props => {
     const { peoples, onPressItem } = props
 
-    const items = peoples.map(people => {
-        return <PeopleListItem 
-        key={people.name.first} 
-        people={people} 
-        navigateToPeopleDetail={onPressItem} />
-    })
-
     return (
-        <ScrollView style={styles.container}>
-            { items }
-        </ScrollView>
+        <FlatList 
+            style={styles.container}
+            data={peoples}
+            renderItem={({ item }) => (
+                <PeopleListItem 
+                    people={item} 
+                    navigateToPeopleDetail={onPressItem} />
+                    )}
+            keyExtractor={item => item.name.first} />
     )
 }
 
