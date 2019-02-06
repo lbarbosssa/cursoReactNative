666e35941a636f50c04d10f2c5a080e85d78ebdc
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-06 09:28:16 -0200

    Estilo no Header

--- seriesApp/App.js
+++ seriesApp/App.js
@@ -4,8 +4,25 @@ import LoginPage from './src/pages/LoginScreen'
 
 const AppNavigator = createStackNavigator ({
   'Login': {
-    screen: LoginPage
+    screen: LoginPage,
+    navigationOptions: {
+      title: 'Bem vindo!'
+    }
   }
+  }, {
+    defaultNavigationOptions: {
+      title: 'Series',
+      headerTintColor: 'white',     
+      headerStyle: {
+        backgroundColor: '#6ca2f7',
+        borderBottomWidth: 1,
+        borderBottomColor: '#c5c5c5'
+      },
+      headerTitleStyle: {
+        color: 'white',
+        fontSize: 30,
+      }
+    }
 })
 
 const AppContainer = createAppContainer(AppNavigator)
