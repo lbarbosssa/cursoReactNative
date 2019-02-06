07cb5c13d2de75937211ffda34badc86171dbf65
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-06 10:40:28 -0200

    Inputs na maneira correta

--- seriesApp/src/pages/LoginScreen.js
+++ seriesApp/src/pages/LoginScreen.js
@@ -4,19 +4,39 @@ import { View, Text, TextInput, StyleSheet } from 'react-native';
 import FormRow from '../components/FormRow'
 
 export default class LoginPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mail: '',
+      password: ''
+    }
+
+  }
+
+  onChangeHandler(field, value){
+    this.setState({
+      [field]: value
+    })
+  }
+
   render() {
     return (
       <View>
         <FormRow>
           <TextInput
-          style={styles.input}
-          placeholder="user@email.com" />
+            style={styles.input}
+            placeholder="user@email.com"
+            value={this.state.mail}
+            onChangeText={value => this.onChangeHandler('mail', value)} />
         </FormRow>
         <FormRow>
           <TextInput
-          style={styles.input}
-          placeholder="Senha super segura" 
-          secureTextEntry={true}/>
+            style={styles.input}
+            placeholder="Senha super segura"
+            value={this.state.password}
+            onChangeText={value => this.onChangeHandler('password', value)}
+            secureTextEntry={true} />
         </FormRow>
       </View>
     );
@@ -24,7 +44,7 @@ export default class LoginPage extends Component {
 }
 
 const styles = StyleSheet.create({
-  input:{
+  input: {
     paddingLeft: 5,
     paddingRight: 5,
     paddingBottom: 5,
