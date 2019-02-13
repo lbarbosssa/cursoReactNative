43c7876af646f681c531a57241bf0934e1ca5533
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-13 09:37:18 -0200

    Drawer Navigation

---
+++ exercicios/src/Menu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+
+import Simples from './componentes/Simples'
+import PareImpar from './componentes/PareImpar'
+import Inverter, { MegaSena } from './componentes/Multi'
+
+import { createDrawerNavigator } from 'react-navigation'
+
+export default createDrawerNavigator({
+    MegaSena:{
+        screen: () => <MegaSena />,
+        navigationOptions: { title: 'Mega Sena'}
+    },
+    Inverter: {
+        screen: () =>  <Inverter texto="React Native" />
+    },
+    PareImpar: {
+        screen: () =>  <PareImpar numero={3}/>,
+        navigationOptions: { title: 'Par & Impar'}
+    },
+    Silpmes: {
+        screen: () =>  <Simples texto="Texto via props"/>
+    },
+    
+}, {drawerWidth: 300})
