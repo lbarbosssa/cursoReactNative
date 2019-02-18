b4fe343c999d118dfa232a4a7c57add9d637e76a
lbarbosssa <barbosa.lucas1996@gmail.com>
2019-02-18 17:45:25 -0300

    Melhorando o componente Button

--- calculator/App.js
+++ calculator/App.js
@@ -8,28 +8,40 @@ export default class App extends Component {
     displayValue: '0'
   }
 
+  addDig = n => {
+    this.setState({displayValue: n})
+  }
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'})
+  }
+
+  setOperation = operation => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Display value={this.state.displayValue}/>
         <View style={styles.buttons}>
-          <Button label='AC' />
-          <Button label='/' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='*' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='-' />
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='+' />
-          <Button label='0' />
-          <Button label='.' />
-          <Button label='=' />
+          <Button label='AC' triple onClick={ this.clearMemory } />
+          <Button label='/' operation onClick={ this.setOperation } />
+          <Button label='7' onClick={ this.addDig } />
+          <Button label='8' onClick={ this.addDig } />
+          <Button label='9' onClick={ this.addDig } />
+          <Button label='*' operation onClick={ this.setOperation } />
+          <Button label='4' onClick={ this.addDig } />
+          <Button label='5' onClick={ this.addDig } />
+          <Button label='6' onClick={ this.addDig } />
+          <Button label='-' operation onClick={ this.setOperation } />
+          <Button label='1' onClick={ this.addDig } />
+          <Button label='2' onClick={ this.addDig } />
+          <Button label='3' onClick={ this.addDig } />
+          <Button label='+' operation onClick={ this.setOperation } />
+          <Button label='0' double onClick={ this.addDig } />
+          <Button label='.' onClick={ this.addDig } />
+          <Button label='=' operation onClick={ this.setOperation } />
         </View>
       </View>
     );
